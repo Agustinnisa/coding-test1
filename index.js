f2887563f5d0099493cd85d73a8f92e0f639207e
Agustinnisa <nisaagustin6@gmail.com>
2024-09-03 21:20:42 +0700

    sudah solved, ternyata ada typo

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-function linierSearch (array, target) {
-    if (typeof target !== 'number') {
-        return 'input harus number'; 
+function linierSeacrh(arr, target) {
+    if (typeof target !== "number") {
+        return "Inputan harus number";
     }
 
-    let 
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] === target) {
-            return i;
+            return target;
         }
     }
+
     return false;
 }
 
-console.log(linierSearch([8, 3, 9, 2, 5], 3));
+console.log(linierSeacrh([8, 3, 9, 2, 5], 3));
+console.log(linierSeacrh([8, 3, 9, 2, 5], 1));
+console.log(linierSeacrh([8, 3, 9, 2, 5], "h"));
